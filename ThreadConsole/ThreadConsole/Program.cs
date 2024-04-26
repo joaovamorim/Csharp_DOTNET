5442b306780bf8cf0a3a37cf5ad2a821dc2606ce
JOAO VICTOR AMORIM <joao.vamorim@embraer.com.br>
2024-04-26 17:17:50 -0300

    Commit parcial aula Thread Parte 1 stop in 10:06 minutes.

---
+++ ThreadConsole/ThreadConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿// using System.Threading;
+
+namespace ThreadConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(Tarefa));
+            thread.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Principal");
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Tarefa()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Tarefa Executada.");
+            }
+        }
+    }
+}
